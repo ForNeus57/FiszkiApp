1762329795b5218e992810ad32a85ebd55a13691
CaIiguIa <sdjurecki@gmail.com>
2023-06-02 09:36:59 +0200

    DB change

--- FiszkiApp/Program.cs
+++ FiszkiApp/Program.cs
@@ -26,6 +26,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-DatabaseConnector.GetInstance();
+DatabaseConnector dc =DatabaseConnector.GetInstance();
 
 app.Run();
